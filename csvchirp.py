854fb3a9824ebd798e48e8169b4ead82b1c49fb4
jkienitz@gmail.com <jkienitz@gmail.com>
2020-11-20 20:25:49 -0800

    new fields

--- csvchirp.py
+++ csvchirp.py
@@ -4,10 +4,12 @@ import csv
 # Channel Number,Receive Frequency,Transmit Frequency,Offset Frequency,Offset Direction,Operating Mode,Name,Show Name,Tone Mode,CTCSS,DCS,Tx Power,Skip,Step,Clock Shift,Bank 1,Bank 2,Bank 3,Bank 4,Bank 5,Bank 6,Bank 7,Bank 8,Bank 9,Bank 10,Comment,Tx Narrow,Pager Enable,
 
 fieldnames = ['Location', 'Name', 'Frequency', 'Duplex', 'Offset', 'Tone', 'rToneFreq', 'cToneFreq', 'DtcsCode', 'DtcsPolarity', 'Mode', 'TStep', 'Skip', 'Comment', 'URCALL', 'RPT1CALL', 'RPT2CALL']
-offsetdirection = {"Minus": '-', "Plus": '+', "Simplex": '', "Split": ''}
-offset = {'5.00 MHz': '5.00', '7.85 MHz': '7.85', '8.00 MHz': '8.00', '9.15 MHz': '9.15', '600 kHz': '0.600000', ' ': ''}
-tone = {'Tone': 'Tone', 'None': ''}
+offsetdirection_values = {"Minus": '-', "Plus": '+', "Simplex": '', "Split": ''}
+offset_values = {'5.00 MHz': '5.00', '7.85 MHz': '7.85', '8.00 MHz': '8.00', '9.15 MHz': '9.15', '600 kHz': '0.600000', ' ': ''}
+tone_values = {'Tone': 'Tone', 'None': ''}
 skip_values = {'Off': '', 'Skip': 'S', 'XXX': 'P'}
+
+counter = 0
 
 with open('XCZFreqListv1_01.csv', newline='') as csvfile:
   reader = csv.DictReader(csvfile)
@@ -18,22 +20,23 @@ with open('XCZFreqListv1_01.csv', newline='') as csvfile:
       outrow = {'Location': row['Channel Number'], 
         'Name': row['Name'], 
         'Frequency': row['Receive Frequency'], 
-        'Duplex': offsetdirection[row['Offset Direction']], 
-        'Offset': offset[row['Offset Frequency']], 
-        'Tone': tone[row['Tone Mode']], 
-        'rToneFreq': row['CTCSS'], 
-        'cToneFreq': row[''], 
+        'Duplex': offsetdirection_values[row['Offset Direction']], 
+        'Offset': offset_values[row['Offset Frequency']], 
+        'Tone': tone_values[row['Tone Mode']], 
+        'rToneFreq': row['CTCSS'].split()[0], 
+        'cToneFreq': '88.5',  # row[''],  # ???
         'DtcsCode': row['DCS'], 
-        'DtcsPolarity': '',  # row[''], 
+        'DtcsPolarity': 'NN',  # row[''],   # ???
         'Mode': 'FM',          # row[''], 
-        'TStep': '',         # row[''], 
+        'TStep': '5.00',         # row[''], 
         'Skip': skip_values[row['Skip']], 
         'Comment': row['Comment'], 
         'URCALL': '',       # row[''], 
         'RPT1CALL': '',     # row[''], 
         'RPT2CALL': ''     # row['']
         }
-      print(outrow.items())
       writer.writerow(outrow)
+      counter += 1
+print(counter)
 
       
