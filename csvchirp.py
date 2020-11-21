de6e15224bdd923a0e4c8c5549eb717e5ebb2cd0
jkienitz@gmail.com <jkienitz@gmail.com>
2020-11-20 19:38:35 -0800

    initial checkin

---
+++ csvchirp.py
@@ -0,0 +1,39 @@
+import csv
+
+# Input fuelds
+# Channel Number,Receive Frequency,Transmit Frequency,Offset Frequency,Offset Direction,Operating Mode,Name,Show Name,Tone Mode,CTCSS,DCS,Tx Power,Skip,Step,Clock Shift,Bank 1,Bank 2,Bank 3,Bank 4,Bank 5,Bank 6,Bank 7,Bank 8,Bank 9,Bank 10,Comment,Tx Narrow,Pager Enable,
+
+fieldnames = ['Location', 'Name', 'Frequency', 'Duplex', 'Offset', 'Tone', 'rToneFreq', 'cToneFreq', 'DtcsCode', 'DtcsPolarity', 'Mode', 'TStep', 'Skip', 'Comment', 'URCALL', 'RPT1CALL', 'RPT2CALL']
+offsetdirection = {"Minus": '-', "Plus": '+', "Simplex": '', "Split": ''}
+offset = {'5.00 MHz': '5.00', '7.85 MHz': '7.85', '8.00 MHz': '8.00', '9.15 MHz': '9.15', '600 kHz': '0.600000', ' ': ''}
+tone = {'Tone': 'Tone', 'None': ''}
+skip_values = {'Off': '', 'Skip': 'S', 'XXX': 'P'}
+
+with open('XCZFreqListv1_01.csv', newline='') as csvfile:
+  reader = csv.DictReader(csvfile)
+  with open('chirp.csv', 'w', newline='') as csvoutput:
+    writer = csv.DictWriter(csvoutput, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in reader:
+      outrow = {'Location': row['Channel Number'], 
+        'Name': row['Name'], 
+        'Frequency': row['Receive Frequency'], 
+        'Duplex': offsetdirection[row['Offset Direction']], 
+        'Offset': offset[row['Offset Frequency']], 
+        'Tone': tone[row['Tone Mode']], 
+        'rToneFreq': row['CTCSS'], 
+        'cToneFreq': row[''], 
+        'DtcsCode': row['DCS'], 
+        'DtcsPolarity': '',  # row[''], 
+        'Mode': 'FM',          # row[''], 
+        'TStep': '',         # row[''], 
+        'Skip': skip_values[row['Skip']], 
+        'Comment': row['Comment'], 
+        'URCALL': '',       # row[''], 
+        'RPT1CALL': '',     # row[''], 
+        'RPT2CALL': ''     # row['']
+        }
+      print(outrow.items())
+      writer.writerow(outrow)
+
+      
